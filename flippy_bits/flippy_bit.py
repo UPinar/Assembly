81374f5d47185076a434fd36377c52c1ca8f6991
Uygar Pinar <uygrpinar@gmail.com>
2024-04-18 02:59:49 +0300

    exception handling

--- flippy_bits/flippy_bit.py
+++ flippy_bits/flippy_bit.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import StaleElementReferenceException
 import numpy as np
 import time
 
@@ -27,25 +28,20 @@ driver.set_window_position(0, 0)
 
 game_container = driver.find_element(By.ID, "game-container")
 body = driver.find_element(By.TAG_NAME, 'body')
-# score_container = driver.find_element(By.ID, "score")
-# game_over_container = driver.find_element(By.ID, "game-over")
+score_container = driver.find_element(By.ID, "score")
 
 
 BINARY_LENGTH = 8
 
 # --------HELPER FUNCTIONS--------
-def hex_to_binary(hex_str):
-  int_val = int(hex_str, 16)
-  return bin(int_val)[2:].zfill(BINARY_LENGTH)
-
-# def control_game_is_over():
-#   if game_over_container.is_displayed():
-#     score = score_container.get_attribute("innerHTML")
-#     print(f"Score is = {score}")
-#   driver.quit()
-#   exit()
+def game_over():
+  score = score_container.get_attribute("innerHTML")
+  print(f"Score is = {score}")
+  driver.quit()
+  exit()
 
 
+# --------MAIN FUNCTION--------
 def main():
   def get_hit_keys_arr(hex_str: str):  
     nonlocal hit_keys
@@ -55,18 +51,18 @@ def main():
     hit_keys = np.array([key for key, bit in zip(keys, binary_array) if bit == 1])
     return hit_keys
   
+
   # --------CLICK TO START--------
   time.sleep(6)
   game_container.click()
 
 
-# --------GAME STARTS--------
+  # --------GAME STARTS--------
   enemies_shot = dict()
   keys = ('1', '2', '3', '4','5', '6', '7', '8')
   hit_keys = np.array([])
 
   while True:
-
     while True:
       visible_enemies = np.array(driver.find_elements(By.XPATH, "//div[starts-with(@id, 'enemy-')]"))
       enemies_to_hit = np.array([enemy for enemy in visible_enemies if enemy not in enemies_shot])
@@ -75,11 +71,15 @@ def main():
           break
 
     for enemy in enemies_to_hit:
-      enemy_hex = enemy.get_attribute("innerHTML")
-      hit_keys_arr = get_hit_keys_arr(enemy_hex)
-      body.send_keys(*hit_keys_arr)
-      hit_keys = np.array([])
-      enemies_shot[enemy] = True
+      # control_game_is_over()
+      try:
+        enemy_hex = enemy.get_attribute("innerHTML")
+        hit_keys_arr = get_hit_keys_arr(enemy_hex)
+        body.send_keys(*hit_keys_arr)
+        hit_keys = np.array([])
+        enemies_shot[enemy] = True
+      except StaleElementReferenceException:
+          game_over()
 
-
+      
 __name__ == '__main__' and main()
